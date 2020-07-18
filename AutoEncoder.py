2a7d1d6879bd2d1e56a930f85852a9ecce94a676
Noam <noams@epd-medical.com>
2020-07-18 20:07:53 +0300

    added validation

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -19,7 +19,7 @@ class AutoEncoder(nn.Module):
 
         self._criterion = nn.MSELoss()
 
-        self._loss = 0
+        self._epoch_loss = 0
         self._path_to_snapshots = "./weights"
         assert os.path.isdir(self._path_to_snapshots)
 
@@ -50,36 +50,39 @@ class AutoEncoder(nn.Module):
 
         n_epochs = 20
         for epoch in range(n_epochs):
-            self._loss = self._train(model, epoch, train_loader, optimizer)
+            self._train(model, epoch, train_loader, optimizer)
+            eval_loss = self._eval(model, epoch, test_loader)
 
             # display the epoch training loss
-            print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, n_epochs, self._loss))
+            print(f"epoch : {epoch + 1}/{n_epochs}, train_loss = {self._epoch_loss:.6f} "
+                  f"validation_loss = {eval_loss:.6f}")
 
     def _train(self, model, epoch, train_loader, optimizer):
+        model.train()
         path_to_weights_file = os.path.join(self._path_to_snapshots, str(epoch) + ".weights")
-        loss = 0
+        self._epoch_loss = 0
         for batch_features, _ in train_loader:
-            # reshape mini-batch data to [N, 784] matrix
-            # load it to the active device
             batch_features = batch_features.view(-1, 784).to(self._device)
-            # reset the gradients back to zero
-            # PyTorch accumulates gradients on subsequent backward passes
             optimizer.zero_grad()
-            # compute reconstructions
             outputs = model(batch_features)
-            # compute training reconstruction loss
-            train_loss = self._criterion(outputs, batch_features)
-            # compute accumulated gradients
-            train_loss.backward()
-            # perform parameter update based on current gradients
+            batch_loss = self._criterion(outputs, batch_features)
+            batch_loss.backward()
             optimizer.step()
-            # add the mini-batch training loss to epoch loss
-            loss += train_loss.item()
-        loss = loss / len(train_loader)
+            self._epoch_loss += batch_loss.item()
+        self._epoch_loss = self._epoch_loss / len(train_loader)
 
         torch.save(model.state_dict(), path_to_weights_file)
-        return loss
 
+    def _eval(self, model, epoch, test_loader):
+        model.eval()
+        eval_loss = 0
+        for batch_features, _ in test_loader:
+            batch_features = batch_features.view(-1, 784).to(self._device)
+            outputs = model(batch_features)
+            batch_loss = self._criterion(outputs, batch_features)
+            eval_loss += batch_loss.item()
+        eval_loss = eval_loss / len(test_loader)
+        return eval_loss
 
 if __name__ == "__main__":
     ae = AutoEncoder(input_shape=784)
